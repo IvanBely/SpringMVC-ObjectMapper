3b2ad37e2991990c2f7e9a4329231d527e17cc97
ivan <belyakov_ivan96@mail.ru>
2024-07-25 17:54:38 +0300

    init

---
+++ src/main/java/com/example/SpringMVC_ObjectMapper/SpringMvcObjectMapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVC_ObjectMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcObjectMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcObjectMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMVC_ObjectMapper/SpringMvcObjectMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVC_ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcObjectMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
